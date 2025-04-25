93cadaa95eaefe5313e218bc68fa82b54ce3b3df
Andrzej Rybczak <andrzej@rybczak.net>
2025-04-25 21:52:14 +0200

    Require resource-pool >= 0.4 and add support for setting number of stripes

--- src/Database/Redis/Connection.hs
+++ src/Database/Redis/Connection.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -14,16 +13,6 @@ import qualified Data.ByteString.Char8 as Char8
 import Data.Functor(void)
 import qualified Data.IntMap.Strict as IntMap
 import Data.Pool
-    ( Pool
-    , withResource
-    , destroyAllResources
-#if MIN_VERSION_resource_pool(0,3,0)
-    , newPool
-    , defaultPoolConfig
-#else
-    , createPool
-#endif
-    )
 import Data.Typeable
 import qualified Data.Time as Time
 import Network.TLS (ClientParams)
@@ -86,6 +75,8 @@ data ConnectInfo = ConnInfo
     , connectMaxConnections :: Int
     -- ^ Maximum number of connections to keep open. The smallest acceptable
     --   value is 1.
+    , connectNumStripes     :: Maybe Int
+    -- ^ Number of stripes in the connection pool.
     , connectMaxIdleTime    :: Time.NominalDiffTime
     -- ^ Amount of time for which an unused connection is kept open. The
     --   smallest acceptable value is 0.5 seconds. If the @timeout@ value in
@@ -115,6 +106,7 @@ instance Exception ConnectError
 --  connectUsername       = Nothing         -- No user
 --  connectDatabase       = 0               -- SELECT database 0
 --  connectMaxConnections = 50              -- Up to 50 connections
+--  connectNumStripes     = Nothing         -- A stripe per cabability
 --  connectMaxIdleTime    = 30              -- Keep open for 30 seconds
 --  connectTimeout        = Nothing         -- Don't add timeout logic
 --  connectTLSParams      = Nothing         -- Do not use TLS
@@ -129,6 +121,7 @@ defaultConnectInfo = ConnInfo
     , connectUsername       = Nothing
     , connectDatabase       = 0
     , connectMaxConnections = 50
+    , connectNumStripes     = Nothing
     , connectMaxIdleTime    = 30
     , connectTimeout        = Nothing
     , connectTLSParams      = Nothing
@@ -167,11 +160,7 @@ createConnection ConnInfo{..} = do
 --  until the first call to the server.
 connect :: ConnectInfo -> IO Connection
 connect cInfo@ConnInfo{..} = NonClusteredConnection <$>
-#if MIN_VERSION_resource_pool(0,3,0)
-    newPool (defaultPoolConfig (createConnection cInfo) PP.disconnect (realToFrac connectMaxIdleTime) connectMaxConnections)
-#else
-    createPool (createConnection cInfo) PP.disconnect 1 connectMaxIdleTime connectMaxConnections
-#endif
+    newPool (setNumStripes connectNumStripes $ defaultPoolConfig (createConnection cInfo) PP.disconnect (realToFrac connectMaxIdleTime) connectMaxConnections)
 
 -- |Constructs a 'Connection' pool to a Redis server designated by the
 --  given 'ConnectInfo', then tests if the server is actually there.
@@ -233,11 +222,7 @@ connectCluster bootstrapConnInfo = do
     case commandInfos of
         Left e -> throwIO $ ClusterConnectError e
         Right infos -> do
-#if MIN_VERSION_resource_pool(0,3,0)
-            pool <- newPool (defaultPoolConfig (Cluster.connect infos shardMapVar Nothing $ connectHooks bootstrapConnInfo) Cluster.disconnect (realToFrac $ connectMaxIdleTime bootstrapConnInfo) (connectMaxConnections bootstrapConnInfo))
-#else
-            pool <- createPool (Cluster.connect infos shardMapVar Nothing $ connectHooks bootstrapConnInfo) Cluster.disconnect 1 (connectMaxIdleTime bootstrapConnInfo) (connectMaxConnections bootstrapConnInfo)
-#endif
+            pool <- newPool (setNumStripes (connectNumStripes bootstrapConnInfo) $ defaultPoolConfig (Cluster.connect infos shardMapVar Nothing $ connectHooks bootstrapConnInfo) Cluster.disconnect (realToFrac $ connectMaxIdleTime bootstrapConnInfo) (connectMaxConnections bootstrapConnInfo))
             return $ ClusteredConnection shardMapVar pool
 
 shardMapFromClusterSlotsResponse :: ClusterSlotsResponse -> IO ShardMap
